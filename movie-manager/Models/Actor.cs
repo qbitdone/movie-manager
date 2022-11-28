a2fdce4fac1856b1468e5e0823128c1c6cb64156
Kristijan <kculina@protonmail.ch>
2022-11-28 22:11:18 +0100

    Add Actor Model

---
+++ movie-manager/Models/Actor.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace movie_manager.Models
+{
+    public class Actor
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Adress { get; set; }
+        [Required]
+        public double ExpectedSalary { get; set; }
+        public string Role { get; set; } = "Actor";
+    }
+}
