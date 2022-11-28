d9a0f67537b05e5611fc7e1062f6973efde059b4
Kristijan <kculina@protonmail.ch>
2022-11-28 23:21:14 +0100

    Add DirectorService

---
+++ movie-manager/Services/DirectorService.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using movie_manager.Data;
+using movie_manager.Models;
+
+namespace movie_manager.Services
+{
+    public class DirectorService
+    {
+        private readonly MovieManagerDbContext _context;
+        private readonly IMapper _mapper;
+
+        public DirectorService(MovieManagerDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<Director>> GetAllDirectors() => await _context.Directors.ToListAsync();
+
+    }
+}
