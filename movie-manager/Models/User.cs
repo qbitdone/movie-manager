77155a4ef225fb079bfeb6ab8e4f183b4ee3eac1
Kristijan <kculina@protonmail.ch>
2022-11-28 16:25:34 +0100

    Update User model with required attributes

--- movie-manager/Models/User.cs
+++ movie-manager/Models/User.cs
@@ -1,11 +1,17 @@
-﻿namespace movie_manager.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace movie_manager.Models
 {
     public class User
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Surname { get; set; }
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Password { get; set; }
         public string Role { get; set; }
     }
--- movie-manager/Program.cs
+++ movie-manager/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddTransient<UserService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
 
