13e951f2339c1c3b918048fba1bd3cf8fc913c48
Kristijan <kculina@protonmail.ch>
2022-11-28 16:28:33 +0100

    Add UserResponse and UserRequest Models

---
+++ movie-manager/Models/UserRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace movie_manager.Models
+{
+    public class UserRequest
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
