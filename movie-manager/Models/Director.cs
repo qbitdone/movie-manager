fc091f240d1a67af1ef51d6e5f171640ab10a32c
Kristijan <kculina@protonmail.ch>
2022-11-28 22:09:44 +0100

    Add Director model

---
+++ movie-manager/Models/Director.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace movie_manager.Models
+{
+    public class Director
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public string Role { get; set; } = "Director";
+    }
+}
