90137f98171cb5da204922b71c31f6065ccb3805
Kristijan <kculina@protonmail.ch>
2022-11-28 13:45:41 +0100

    Add Korisnik model

---
+++ movie-manager/Models/Korisnik.cs
@@ -0,0 +1,12 @@
+﻿namespace movie_manager.Models
+{
+    public class Korisnik
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string KorisnickoIme { get; set; }
+        public string Lozinka { get; set; }
+        public string Uloga { get; set; }
+    }
+}
