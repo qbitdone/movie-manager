f2fb6b1e00531cab0f8c21506ce63313f459630a
Kristijan <kculina@protonmail.ch>
2022-11-29 14:02:39 +0100

    Add MovieRequest and MovieResponse models

---
+++ movie-manager/Models/MovieRequest.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace movie_manager.Models
+{
+    public class MovieRequest
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Duration { get; set; }
+        [Required]
+        public double Budget { get; set; }
+        [Required]
+        public DateTime StartRecording { get; set; }
+        [Required]
+        public DateTime EndRecording { get; set; }
+        [Required]
+        public Guid DirectorId { get; set; }
+    }
+}
